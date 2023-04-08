4fa650557e6352e3e4065f156f3cb3e61176e0a5
Peter Njiru <peternjirum@gmail.com>
2023-04-08 15:02:43 -0400

    completed js event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const btn = document.getElementById("button")
+    btn.addEventListener("click", function () { alert("I was clicked!") })
 }
+
+addingEventListener()
